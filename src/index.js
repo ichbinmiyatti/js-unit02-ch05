f51c2f2ad915334228883ab75ca4571ad512d6e1
Yusuke Miyaura <ichheisseyuhakase@gmail.com>
2019-05-08 18:16:52 +0900

    changed

--- src/index.js
+++ src/index.js
@@ -1,45 +1,43 @@
-import Polyglot from 'node-polyglot';
+import Polyglot from "node-polyglot";
 
 class TranslationApp {
   constructor() {
     this.polyglot = new Polyglot();
-    this.currentLocale = localStorage.getItem('locale');
-
+    this.currentLocale = localStorage.getItem("locale") || "ja";
+    this.updateLocale = this.updateLocale.bind(this);
   }
 
   setup() {
-    /*
-      現在のLocaleに合わせて、polyglotにメッセージをセットします。
-      メッセージのセットにはpolyglot.extend()を利用します。
-    */
-    const currentLocale = this.currentLocale
-    this.polyglot.extend({
-      'hello': 'こんにちは、世界'
-    });
+    const currentLocale = this.currentLocale;
+    if (currentLocale === "ja") {
+      this.polyglot.extend({
+        hello: "こんにちは、世界"
+      });
+    } else {
+      this.polyglot.extend({
+        hello: "Hello World!"
+      });
+    }
   }
 
   updateLocale(e) {
     /*
       ボタンにセットされたdata-localeを元に現在のlocaleを変更します。
     */
-
   }
 
   showMessage() {
-      /*
-        mainというidがセットされた要素の下にh1タグで現在のlocaleに応じて、メッセージを表示します。
-      */
-      const mainEl = document.getElementById('main');
-      mainEl.innerHTML = `<h1>${this.polyglot.t('hello')</h1>`
+    const mainEl = document.getElementById("main");
+    mainEl.innerHTML = `
+      <h1>${this.polyglot.t("hello")}</h1>
+    `;
   }
-
 }
 
-
 {
-  const button1 = document.getElementById('button1');
+  const button1 = document.getElementById("button1");
   button1.addEventListener("click", app.updateLocale);
 
-  const button2 = document.getElementById('button2');
+  const button2 = document.getElementById("button2");
   button2.addEventListener("click", app.updateLocale);
 }
